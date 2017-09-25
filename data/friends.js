7eae4d922790375c0007db6fb018b8986c374932
Cristina Salvo <csalvo783@gmail.com>
2017-09-25 12:58:54 -0400

    display survey results in modal

--- data/friends.js
+++ data/friends.js
@@ -1,7 +1,7 @@
 var friends = [
 {
 	"name": "Default Friend",
-	"photo": "http://google.com",
+	"photo": "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Cartoon_Robot.svg/868px-Cartoon_Robot.svg.png",
 	"scores":[1,2,3,4,5,1,2,3,4,5]
 }];
 
