9037f1d80cc1bb756ff2a0834ace42f9d2b89d83
Cristina Salvo <csalvo@spendsetter.com>
2017-09-24 22:48:25 -0400

    friend finding logic

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -8,18 +8,40 @@ module.exports = function(app) {
 
     app.post("/api/friends", function(req, res) {
         friends.push(req.body);
-        for (var i = 0; i < friends.length; i++) {
-        	friends[i]
+        currentFriendScore = 100;
+
+        for (var i = 0; i < friends.length - 1; i++) {
+            findFriend(req.body, friends[i]);
         }
+        res.json(currentBf);
+        console.log(currentBf);
     });
 }
 
 
-// var currentBfName;
-// var currentBfScore;
-// var currentBfPic;
-// var friendScore;
+
 
 // for each friend, loop through each scores array, 
 //  subtracting each number from body.scores then add it to friend scores
 //  if friendScore is greater than currentBfScore set currentBf to friend[i].name and currentBfPic  
+
+var findFriend = function(newUser, friend) {
+	console.log(friend);
+var friendScore = 0;
+
+    for (var i = 0; i < friend.scores.length; i++) {
+        friendScore += Math.abs(friend.scores[i] - newUser.scores[i]);
+   console.log(friendScore);
+    }
+
+       getBestFriend(friend, friendScore);
+}
+
+var getBestFriend = function(friend, thisFriendsScore) {
+
+    if (thisFriendsScore < currentFriendScore) {
+        currentFriendScore = thisFriendsScore;
+        currentBf = friend;
+        return (currentBf);
+    }
+}
